d57c287921eccc287d7cd4e3d8972e174e21e16f
Evan Hong <evangelurtz@gmail.com>
2020-05-16 21:49:48 +0900

    Add root index file

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 4000;
+const server = app.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
+});
+
+module.exports = server;
