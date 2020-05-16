d2737fe1eee12c90e2fe7a5cc680f3056070f141
Evan Hong <evangelurtz@gmail.com>
2020-05-17 00:08:52 +0900

    Add user model and auth route

---
+++ src/models/user.ts
@@ -0,0 +1,62 @@
+import config = require('config');
+import jwt = require('jsonwebtoken');
+import Joi = require('joi');
+import mongoose = require('mongoose');
+import mongodb = require('mongodb');
+
+interface UserType {
+    name: string;
+    username: string;
+    password: string;
+}
+
+interface UserInterface extends mongoose.Document {
+    name: string;
+    username: string;
+    password: string;
+    isAdmin: boolean;
+    _id: mongodb.ObjectId;
+    generateAuthToken: Function;
+}
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50,
+    },
+    username: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 255,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 1024,
+    },
+    isAdmin: Boolean,
+});
+
+userSchema.methods.generateAuthToken = function () {
+    const token = jwt.sign({ _id: this._id, isAdmin: this.isAdmin }, config.get('jwtPrivateKey'));
+    return token;
+};
+
+const User = mongoose.model<UserInterface>('User', userSchema);
+
+const validateUser = (user: UserType) => {
+    const schema = {
+        name: Joi.string().min(5).max(50).required(),
+        username: Joi.string().min(5).max(255).required(),
+        password: Joi.string().min(5).max(255).required(),
+    };
+
+    return Joi.validate(user, schema);
+};
+
+export { UserInterface, User, validateUser as validate };
---
+++ src/routes/auth.ts
@@ -0,0 +1,37 @@
+import Joi = require('joi');
+import bcrypt = require('bcrypt');
+import { User } from '../models/user';
+import express = require('express');
+
+const router = express.Router();
+
+const validate = (req: express.Request) => {
+    const schema = {
+        username: Joi.string().min(5).max(255).required(),
+        password: Joi.string().min(5).max(255).required(),
+    };
+
+    return Joi.validate(req, schema);
+};
+
+router.post('/', async (req: express.Request, res: express.Response) => {
+    const { error } = validate(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) {
+        return res.status(400).send('Invalid username or password.');
+    }
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) {
+        return res.status(400).send('Invalid username or password.');
+    }
+
+    const token = user.generateAuthToken();
+    res.send(token);
+});
+
+export default router;
